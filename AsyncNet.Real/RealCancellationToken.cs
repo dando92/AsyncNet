02883bd7cd90b3d5aef75d51e37ec2c1dc427d39
dando92 <alessandro.dore@live.it>
2026-03-17 14:29:57 +0100

    Improved Real cancellation token implementation

--- AsyncNet.Real/RealCancellationToken.cs
+++ AsyncNet.Real/RealCancellationToken.cs
@@ -6,36 +6,31 @@ namespace AsyncNet.Real
     public class RealCancellationToken : ICancellationToken
     {
         private readonly CancellationTokenSource _cts;
+        private readonly Task _cancellationTask;
+        private readonly CancellationTokenRegistration _registration;
 
         public RealCancellationToken(int expireTime)
         {
-            if (expireTime > 0)
-                _cts = new CancellationTokenSource(expireTime);
-            else
-                _cts = new CancellationTokenSource();
+            _cts = expireTime > 0
+                ? new CancellationTokenSource(expireTime)
+                : new CancellationTokenSource();
+
+            var tcs = new TaskCompletionSource<object>(TaskCreationOptions.RunContinuationsAsynchronously);
+
+            _registration = _cts.Token.Register(() => tcs.TrySetCanceled(_cts.Token));
+
+            _cancellationTask = tcs.Task;
         }
 
-        public Task Task
-        {
-            get
-            {
-                var tcs = new TaskCompletionSource<object>(TaskCreationOptions.RunContinuationsAsynchronously);
-
-                _cts.Token.Register(() => tcs.TrySetCanceled(_cts.Token));
-
-                return tcs.Task;
-            }
-        }
+        public Task Task => _cancellationTask;
 
         public bool IsCancellationRequested => _cts.IsCancellationRequested;
 
-        public void Cancel()
-        {
-            _cts.Cancel();
-        }
+        public void Cancel() => _cts.Cancel();
 
         public void Dispose()
         {
+            _registration.Dispose();
             _cts.Dispose();
         }
     }
